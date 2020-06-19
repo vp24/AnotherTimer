716eb9d4854bb14e4974f81254197e86014d39c7
dishonestea <vishay8@gmail.com>
2020-06-18 22:14:55 -0500

    Updates

---
+++ precache-manifest.0a25276e21aedf3d14f372a3a039e05d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ac5203ac1b8ad6556415cd716427cb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5572e6ad56fdae8c3079",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "9d8f7fb2ad862fddeb03",
+    "url": "/static/css/main.8658571f.chunk.css"
+  },
+  {
+    "revision": "5572e6ad56fdae8c3079",
+    "url": "/static/js/2.c9849f50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c9849f50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d8f7fb2ad862fddeb03",
+    "url": "/static/js/main.41bd7290.chunk.js"
+  },
+  {
+    "revision": "89ac35b4ba8ca899f16a",
+    "url": "/static/js/runtime-main.7f7132dd.js"
+  },
+  {
+    "revision": "2ed3ed02602216d67906c86fa9a3bf15",
+    "url": "/static/media/ting.2ed3ed02.wav"
+  }
+]);
